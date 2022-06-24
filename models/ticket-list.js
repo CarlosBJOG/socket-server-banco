b18b8c5c3624f23ef55bbf22b1f9e160ba5cee8e
Terry <terryxbt@gmail.com>
2022-06-24 01:17:46 -0500

    first commit

---
+++ models/ticket-list.js
@@ -0,0 +1,50 @@
+
+const Ticket = require('./ticket');
+
+
+class TicketList {
+
+    constructor() {
+        this.ultimoNumero = 0;
+
+        this.pendientes = [];
+        this.asignados  = [];
+    }
+
+    get siguienteNumero() {
+        this.ultimoNumero ++;
+        return this.ultimoNumero;
+    }
+
+    //retornar ultimos 3 tickets en tarjetas y 10 historial
+    get ultimos13(){
+        return this.asignados.slice(0, 13);
+    }
+
+    crearTicket(){
+        const nuevoTicket = new Ticket( this.siguienteNumero );
+        this.pendientes.push( nuevoTicket );
+
+        return nuevoTicket;
+    }
+
+    asignarTicket( agente, escritorio ){
+
+        if( this.pendientes.length === 0 ){
+            return null;
+        }
+
+        const siguienteTicket = this.pendientes.shift();
+
+        siguienteTicket.agente = agente;
+        siguienteTicket.escritorio = escritorio;
+
+        this.asignados.unshift( siguienteTicket );
+
+        return siguienteTicket;
+    }
+
+
+}
+
+module.exports = TicketList;
